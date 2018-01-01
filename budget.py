dca1a024e87b53c5853a75268a549bf185dcd227
Zenarim <jlwong092014@gmail.com>
2018-01-01 00:27:08 -0800

    Initial commit.

---
+++ budget.py
@@ -0,0 +1,17 @@
+
+
+class Budget():
+    def __init__(self, balance=0.0):
+        self._balance = balance
+        self.categories = []
+
+    def add_category(self, category):
+        self.categories.append(category)
+
+    def remove_category(self, category_name):
+        for id, category in enumerate(self.categories):
+            if category.name == category_name:
+                del self.categories[id]
+
+    def get_balance(self):
+        return self._balance - sum(category.get_balance() for category in self.categories)
---
+++ tests/test_budget.py
@@ -0,0 +1,40 @@
+import unittest
+import unittest.mock as umock
+
+from budget import Budget
+
+
+class TestBudgetApp(unittest.TestCase):
+    def setUp(self):
+        self.fakeCategory = umock.Mock()
+        self.fakeCategory.name = 'Fake Category'
+        self.fakeCategory.get_balance = umock.Mock(return_value=500.0)
+        self.my_budget = Budget()
+
+    def test_can_add_categories_to_budget(self):
+        my_budget = Budget()
+        my_budget.add_category(self.fakeCategory)
+        self.assertEqual(1, len(my_budget.categories))
+
+    def test_can_remove_categories_to_budget(self):
+        self.my_budget.add_category(self.fakeCategory)
+        self.my_budget.remove_category("Fake Category")
+        self.assertEqual(0, len(self.my_budget.categories))
+
+    def test_budget_will_have_default_balance_of_zero(self):
+        self.assertEqual(0.0, self.my_budget.get_balance())
+
+    def test_budget_can_set_default_balance(self):
+        default_bal = 777.0
+        my_budget = Budget(balance=default_bal)
+        self.assertEqual(default_bal, my_budget.get_balance())
+
+    def test_budget_will_return_difference_between_balance_and_total_balance_of_all_categories(self):
+        self.my_budget.add_category(self.fakeCategory)
+        self.my_budget.add_category(self.fakeCategory)
+        self.my_budget.add_category(self.fakeCategory)
+        self.assertEqual(-1500.0, self.my_budget.get_balance())
+
+
+if '__name__' == '__main__':
+    unittest.main()
