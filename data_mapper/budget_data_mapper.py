c72b0f4990ca5d5f3d1fb6c8f38f05da284059e2
Zenarim <jon.zen.brand@gmail.com>
2018-01-21 14:46:45 -0800

    Add standard insert, update, delete commands for data layer interaction.

--- data_mapper/budget_data_mapper.py
+++ data_mapper/budget_data_mapper.py
@@ -2,7 +2,15 @@
 
 class BudgetDataMapper:
     @staticmethod
-    def insert_statement(key, budget_name, budget_date_created):
+    def insert_statement(budget_name, budget_date_created):
         if not budget_name:
             budget_name = 'Untitled Budget'
-        return f"INSERT INTO budget VALUES {key} \"{budget_name}\" {budget_date_created}"
+        return f"INSERT INTO budget VALUES \"{budget_name}\" {budget_date_created}"
+
+    @staticmethod
+    def update_statement(budget_name, budget_key):
+        return f"UPDATE budget SET budgetName = '{budget_name}' WHERE budgetKey = {budget_key}"
+
+    @staticmethod
+    def delete_statement(budget_key):
+        return f"DELETE FROM budget WHERE budgetKey = {budget_key}"
--- tests/unit_tests/test_budget_data_mapper.py
+++ tests/unit_tests/test_budget_data_mapper.py
@@ -6,8 +6,16 @@ from data_mapper.budget_data_mapper import BudgetDataMapper
 
 class TestBudgetDataMapper(unittest.TestCase):
     def test_budget_insert_statement(self):
-        expected_sql_statement = "INSERT INTO budget VALUES 1 \"Untitled Budget\" 2018-01-01"
-        sql_insert_statement = BudgetDataMapper.insert_statement(key=1, budget_name=None,
-                                                                 budget_date_created="2018-01-01")
+        expected_sql_statement = "INSERT INTO budget VALUES \"Untitled Budget\" 2018-01-01"
+        sql_insert_statement = BudgetDataMapper.insert_statement(budget_name=None, budget_date_created="2018-01-01")
         self.assertEqual(expected_sql_statement, sql_insert_statement)
 
+    def test_budget_update_statement(self):
+        expected_sql_statement = "UPDATE budget SET budgetName = 'Titled Budget' WHERE budgetKey = 1"
+        sql_update_statement = BudgetDataMapper.update_statement(budget_name='Titled Budget', budget_key=1)
+        self.assertEqual(expected_sql_statement, sql_update_statement)
+
+    def test_budget_delete_statement(self):
+        expected_sql_statement = "DELETE FROM budget WHERE budgetKey = 1"
+        sql_delete_statement = BudgetDataMapper.delete_statement(1)
+        self.assertEqual(expected_sql_statement, sql_delete_statement)
